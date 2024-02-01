56f4282656a35c7c8926f9223b5d97a93d1bd202
DaniM0613 <wwh.rj.92@gmail.com>
2024-02-01 11:38:34 -0600

    obtener un proyecto por und ID

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 import express from "express";
 import dotenv from 'dotenv';
 import conectarDB from "./config/db.js";
-import usuarioRoutes from './routes/usuarioRoutes.js'
+import usuarioRoutes from './routes/usuarioRoutes.js';
+import proyectoRoutes from './routes/proyectoRoutes.js';
+
 
 const app = express();
 app.use(express.json());
@@ -13,7 +15,7 @@ conectarDB();
 
 // Routing
 app.use('/api/usuarios', usuarioRoutes);
-
+app.use('/api/proyectos', proyectoRoutes);
 
 const PORT = process.env.PORT || 4000;
 
